ca82dd279b1a96f62d8c3eb3b4a8932947b47bbb
htaussig <33874681+htaussig@users.noreply.github.com>
2018-02-24 11:15:49 -0500

    Update and rename Main to main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main{
+
+  private ArrayList<Prompt> = new ArrayList<Prompt>();
+  private int Stress;
+  private int Energy;
+  private int Friends;
+  private int Grades;
+
+  public static void main(String args[]){
+
+    displayPrompt();
+  
+
+  }
+
+}
