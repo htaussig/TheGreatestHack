a0514f648d6f57623d0a06e4a2407e5c59fb4b7e
AllanYermolenko <33874788+AllanYermolenko@users.noreply.github.com>
2018-02-24 11:50:49 -0500

    add first prompt
    blah

--- main.java
+++ main.java
@@ -2,18 +2,17 @@ import java.util.ArrayList;
 
 public class main{
 
-  private ArrayList<Prompt> prompts = new ArrayList<>();
+  private static ArrayList<Prompt> prompts = new ArrayList<>();
   private int Stress;
   private int Energy;
   private int Friends;
   private int Grades;
 
   public static void main(String args[]){
-
-    displayPrompt();
-  	checking if this works
+	  prompts.add(new Prompt("You did your homework until 1 am. When do you wake up?", "yes", "no", -5, 10, -15, 5, 0, -5, 10, -10));
+	
+	  System.out.println(prompts.get(0));
 
 
   }
-
 }
