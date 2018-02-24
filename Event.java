3c0c703d33412fee094357433b595e2026c11479
michaelalperovich <misha.alperovich1@gmail.com>
2018-02-24 11:11:13 -0500

    Event class created

---
+++ Event.java
@@ -0,0 +1,12 @@
+class Event {
+    String prompt;
+    int stress, energy, grades, friends;
+
+    public Event(String prompt, int stress, int energy, int grades, int friends) {
+        this.prompt = prompt;
+        this.stress = stress;
+        this.energy = energy;
+        this.grades = grades;
+        this.friends = friends;
+    }
+}
